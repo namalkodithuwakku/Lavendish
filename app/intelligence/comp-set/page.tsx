e321912a125a32205e1194876fe425abcebb615f
namalkodithuwakku <namal.kodithuwakku@gmail.com>
2026-08-16 16:53:33 +0530

    Add Comp Set performance page

---
+++ app/intelligence/comp-set/page.tsx
@@ -0,0 +1,5 @@
+import { IntelligenceShell } from "../intelligence-shell";
+import { CompSetClient } from "./comp-set-client";
+import "./comp-set.css";
+
+export default function CompSetPage(){return <IntelligenceShell eyebrow="PERFORMANCE · MARKET POSITION" title="Comp Set"><CompSetClient/></IntelligenceShell>}
